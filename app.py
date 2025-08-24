f37e450a069d5e139530af28ecd260102a8a1765
Shotaro Ishiguro <tennisshow20011218@gmail.com>
2025-08-24 18:43:35 +0900

    Update response message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python running on Amazon ECS!"
+    return "Hello from Python! Deployment was successful!"
 
 if __name__ == '__main__':
     # ポート8000でリッスン
