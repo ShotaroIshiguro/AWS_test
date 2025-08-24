f471e2f4a7b126b37502b3776b277cddf3439a50
Shotaro Ishiguro <tennisshow20011218@gmail.com>
2025-08-24 20:22:23 +0900

    Update response message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Python! Deployment was successful!!!!"
+    return "Hello from Python! Deployment was successful!!!!!"
 
 if __name__ == '__main__':
     # ポート8000でリッスン
