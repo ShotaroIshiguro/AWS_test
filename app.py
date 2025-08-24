48440a877f4ac59c9839ecb61c246dcdbd4a928b
石黒 将太郎 <tennisshow20011218@gmail.com>
2025-08-24 18:22:15 +0900

    Initialize Flask app with hello route

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hello from Python running on Amazon ECS!"
+
+if __name__ == '__main__':
+    # ポート8000でリッスン
+    app.run(host='0.0.0.0', port=8000)
